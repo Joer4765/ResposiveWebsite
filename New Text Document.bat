04bbad8fb4d8db8684d23e0ab01f23d552818b41
Joer4765 <joer.tuhn@gmail.com>
2024-01-05 13:28:38 +0200

    init

---
+++ New Text Document.bat
@@ -0,0 +1,11 @@
+echo "# ResposiveWebsite" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Joer4765/ResposiveWebsite.git
+git push -u origin main
+git add .
+git commit -am "init"
+git push
+pause
